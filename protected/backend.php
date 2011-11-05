d63ebce6d100bdbdf06c38081312275b8d243977
isnowfy <isnowfy@gmail.com>
2011-11-05 21:38:36 +0800

    fix some problem

--- protected/backend.php
+++ protected/backend.php
@@ -1,15 +1,15 @@
 <?php
 
 require_once "dbcon.php";
-$msg = isset($_GET['msg']) ? $_GET['msg'] : '';
-$room = isset($_GET['room']) ? $_GET['room'] : '';
+$msg = isset($_POST['msg']) ? $_POST['msg'] : '';
+$room = isset($_POST['room']) ? $_POST['room'] : '';
 $time = date("Y-m-d H:i:s", time() + 0 * 3600);
-$id = isset($_GET['id']) ? $_GET['id'] : '';
+$id = isset($_POST['id']) ? $_POST['id'] : '';
 $stamp=microtime(TRUE);
 require_once "getsafe.php";
 $id= PAPI_GetSafeParam($id, "", XH_PARAM_TXT);
 $room= PAPI_GetSafeParam($room, "", XH_PARAM_TXT);
-$msg= PAPI_GetSafeParam($msg, "", XH_PARAM_TXT);
+//$msg= PAPI_GetSafeParam($msg, "", XH_PARAM_TXT);
 if ($msg != '')
 {
 	$sql="INSERT INTO record (room,user,time,content,timestamp) VALUES ('$room','$id','$time','$msg','$stamp')";    
@@ -22,9 +22,9 @@ if ($msg != '')
 	flush();
 	die();
 }
-$time= isset($_GET['time']) ? $_GET['time'] : '';
+$time= isset($_POST['time']) ? $_POST['time'] : '';
 $time= PAPI_GetSafeParam($time, "", XH_PARAM_TXT);
-$stamp= isset($_GET['stamp']) ? $_GET['stamp'] : '';
+$stamp= isset($_POST['stamp']) ? $_POST['stamp'] : '';
 $stamp= PAPI_GetSafeParam($stamp, "", XH_PARAM_TXT);
 $sql="select * from record where timestamp>'$stamp' and room='$room' order by timestamp ASC";    
 $result=@mysql_query($sql);
