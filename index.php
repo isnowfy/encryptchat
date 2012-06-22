3e066d62d9fe2c0b156ba5288e82e8794e699323
isnowfy <isnowfy@gmail.com>
2012-06-22 15:17:48 +0000

    modify style

--- index.php
+++ index.php
@@ -1,30 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
-<head> 
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 <title>Chat room</title> 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<script type="text/javascript" src="http://crypto-js.googlecode.com/files/2.3.0-crypto-md5.js"></script>
-<script type="text/javascript" src="http://crypto-js.googlecode.com/files/2.4.0-crypto-sha1-hmac-pbkdf2-blockmodes-aes.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script> 
+<style type="text/css">html,body{
+ width:100%;
+ height:100%;
+ margin:0px;
+ padding:0px;
+ background-color:#F0F7FF;
+}
+</style>
+<script src="crypto.js" type="text/javascript"></script>
+<script src="http://cdn.jqmobi.com/jq.mobi.min.js" type="text/javascript"></script> 
 <script> 
 room="";
 user="";
 password=""
 function encrypt(myuser,mykey){
 	user=myuser;
-	password=mykey;
-	$('#pass').fadeOut();
-	$('#send').fadeIn();
-	room=Crypto.MD5(mykey,{asString:true});
+	password=Crypto.MD5(mykey+"password");
+	$('#pass').toggle();
+	$('#ss').toggle();
+        $('#content').toggle();
+	room=Crypto.MD5(mykey);
 	chat('<?php echo date("Y-m-d H:i:s", time() + 0 * 3600);?>','<?php echo microtime(TRUE);?>');
-}
-function htmlEncode(value){
-	return $('<div/>').text(value).html();
 }
 function doRequest(msg) {
     $.ajax({
-	   type: "GET",
-	   data: "id="+user+"&room="+room+"&msg="+Crypto.AES.encrypt(msg,password),
+	   type: "POST",
+	   data: {id:user,room:room,msg:Aes.Ctr.encrypt(msg,password,256)},
 	   dataType: "json",
 	   url: "protected/backend.php",
 	   success: function(msg){
@@ -33,15 +38,18 @@ function doRequest(msg) {
 }
 function chat(timestamp,stamp) {
 	$.ajax({
-	   type: "GET",
-	   data: "time="+timestamp+"&room="+room+"&stamp="+stamp,
+	   type: "POST",
+	   data: {time:timestamp,room:room,stamp:stamp},
 	   dataType: "json",
 	   url: "protected/backend.php",
 	   success: function(msg){
 		 chat(msg.timestamp,msg.stamp);
 		 var timestamp=msg.timestamp;
 		 var stamp=msg.stamp;
-		 $('#content').append('<div><em>'+timestamp+' <font color="#FF0000">'+msg.user+':</font></em>' + Crypto.AES.decrypt(msg.msg,password) + '</div>');
+		 div=document.getElementById('content');
+		 div.innerHTML=div.innerHTML+'<div><em>'+timestamp+' <font color="#FF0000">'+msg.user+':</font></em>' + 
+		 Aes.Ctr.decrypt(msg.msg,password,256) + '</div>';
+		 div.scrollTop=div.scrollHeight;
 	   },
 	   error: function(msg){
 		 chat(timestamp,stamp);
@@ -51,19 +59,18 @@ function chat(timestamp,stamp) {
 </script> 
 </head> 
 <body> 
-<div class="wrapper">
-<span id="content" style="display:block;width:400px;height:300px;overflow:auto"></span> 
-<p> 
-    <form id="send" style="width:300px;display:none" action="" method="get" onsubmit="var tmp=$('#word').val().replace(/\?/g,'？');doRequest(tmp);$('#word').val('');return false;"> 
-      <input style="width:250px" type="text" name="word" id="word" value="" /> 
-      <input type="submit" name="submit" value="Send" /> 
-    </form> 
-    <form id="pass" action="" method="get" onsubmit="encrypt($('#user').val(),$('#pass').val());return false;"> 
-      pass:<input type="text" name="pass" id="pass" value="" /> 
-      user:<input type="text" name="user" id="user" value="" />
+<div>
+    <form id="pass" action="" method="get" onsubmit="encrypt($('#user').val(),$('#pass0').val());return false;"> 
+      pass:<input type="text" name="pass" id="pass0" value="" /><br /> 
+      user:<input type="text" name="user" id="user" value="" /><br />
       <input type="submit" name="submit" value="save" /> 
     </form> 
-</p> 
+</div>
+<div id="content" style="display:none;width:100%;overflow:auto;position:absolute;bottom:25px;top:0;"></div> 
+<div id="ss" style="width:100%;display:none;position:absolute;bottom:0;border-up:1px solid #D9D9D9;">
+    <form id="send" action="" method="get" onsubmit="var tmp=$('#word').val();doRequest(tmp);$('#word').val('');return false;"> 
+      <input style="width:99%;" type="text" name="word" id="word" value="" /> 
+    </form> 
 </div>
 </body> 
 </html>
